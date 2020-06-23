2b447d541d9c7af0fa2586df1b00ceb9cbb6475a
thanhdc63414 <dangcongthanh1998@gmail.com>
2020-06-24 01:58:14 +0700

    no message

--- app/src/main/java/com/demo/fmalc_android/retrofit/RetrofitInstance.java
+++ app/src/main/java/com/demo/fmalc_android/retrofit/RetrofitInstance.java
@@ -14,7 +14,7 @@ public class RetrofitInstance {
 
     private static Retrofit retrofit;
     private static Gson gson;
-    private static final String BASE_URL = "http:// 192.168.1.28:8080/fmalc/";
+    private static final String BASE_URL = "http://192.168.0.103:8080/fmalc/";
     public static synchronized Retrofit getInstance() {
 
         OkHttpClient.Builder clientBuilder = new OkHttpClient.Builder()
