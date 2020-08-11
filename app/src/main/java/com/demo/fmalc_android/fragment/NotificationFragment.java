8758f98aacdc25efd1562b4845ca18e735523d5d
thanhdc63414 <dangcongthanh1998@gmail.com>
2020-08-12 03:07:38 +0700

    no message

--- app/src/main/java/com/demo/fmalc_android/fragment/NotificationFragment.java
+++ app/src/main/java/com/demo/fmalc_android/fragment/NotificationFragment.java
@@ -63,7 +63,7 @@ public class NotificationFragment extends Fragment implements NotificationMobile
 
         init();
 
-        notificationMobilePresenter.findNotificationByDriverId(globalVariable.getUsername(), globalVariable.getToken());
+        notificationMobilePresenter.findNotificationByUsername(globalVariable.getUsername(), globalVariable.getToken());
 
         swipeRefreshLayout = (SwipeRefreshLayout) view.findViewById(R.id.swipeRefreshLayoutNotification);
         swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
@@ -209,7 +209,7 @@ public class NotificationFragment extends Fragment implements NotificationMobile
             @Override public void run() {
                 List<NotificationMobileResponse> notificationMobileResponses = new ArrayList<>();
                 List<NotificationMobileResponse> showData = new ArrayList<>();
-                notificationMobilePresenter.findNotificationByDriverId(globalVariable.getUsername(), globalVariable.getToken());
+                notificationMobilePresenter.findNotificationByUsername(globalVariable.getUsername(), globalVariable.getToken());
                 swipeRefreshLayout.setRefreshing(false);
             }
         }, 800);
